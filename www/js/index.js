6a0a382e918a8b5a959ce280c755951ef81a0600
arguv <gurgen.aramian@gmail.com>
2015-03-15 16:44:37 +0400

    added code

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,68 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+$(function () {
+    create_new_list();
+});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+var i = localStorage.length;
 
-        console.log('Received Event: ' + id);
+function save_todo() {
+    var todo = $("#input_title").val();
+
+    if (todo.length) {
+
+        localStorage[i] = todo;
+
+        $("#todo_list").append('<li id="' + i + '"><a href="#popup" onClick="get_id(this);">' + todo + '</a></li>');
+
+        $("#todo_list").listview();
+        $("#todo_list").listview("refresh");
+        i++;
     }
-};
+}
 
-app.initialize();
+function create_new_list() {
+
+    for (var i = 0; i < localStorage.length; i++) {
+
+        todo = localStorage.getItem(i);
+
+        $("#todo_list").append('<li id="' + i + '"><a href="#popup" onClick="get_id(this);">' + todo + '</a></li>');
+    }
+
+    $("#todo_list").listview();
+    $("#todo_list").listview("refresh");
+}
+
+function del_todo(e) {
+
+    localStorage.removeItem(this.item);
+
+    for (i = 0; i < localStorage.length; i++) {
+        if (!localStorage.getItem(i)) {
+            localStorage.setItem(i, localStorage.getItem(i + 1));
+            localStorage.removeItem(i + 1);
+        }
+    }
+
+    $("#todo_list").html("");
+
+    create_new_list();
+}
+
+function get_id(e) {
+
+    this.item = $(e).closest("li").attr("id");
+    this.val = localStorage.getItem(this.item);
+
+}
+
+function done_todo(e) {
+
+    this.val = localStorage.getItem(this.item);
+    if ((this.val.indexOf("<del>") == -1)) {
+        localStorage.setItem(this.item, '<del>' + this.val + '</del>');
+    }
+
+    $("#todo_list").html("");
+
+    create_new_list();
+}
